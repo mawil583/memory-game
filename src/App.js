5e555660b3564d73f2734f7f929c7aaf7fc449eb
Michael Williams <mwilliams2964@gmail.com>
2020-03-25 20:03:54 -0700

    added title and game instructions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
 import characters from './characters.json';
 import Card from './components/card';
-import Wrapper from './components/wrapper'
+import Wrapper from './components/wrapper';
+import Jumbotron from './components/jumbotron';
 import './App.css';
 import Title from './components/title';
-import Modal from './components/modal';
-
-
-
-// function shuffle () {
-//   characters.sort(() => {Math.random() - 0.5})
-// }
 
 class App extends Component {
   state = {
@@ -29,85 +23,54 @@ class App extends Component {
 
   }
 
-  
-
-handleClick = id => {
-  // Filter this.state.friends for friends with an id not equal to the id being removed
-  console.log(id);
-  // shuffle();
-  // let hasBeenClickedCopy = [...hasBeenClicked];
-  if (this.state.idArr.includes(id)) {
-    // write "game over" code
-    // filter = (id) => 
-    // id === this.state.characters.id
-    alert(`Game over! You already chose ${this.state.characters.find(character => character.id === id).name}`)
-    // $('#myModal').modal('show');
-    // this.state.gameOver = true;
-    // let gameOver = this.state.gameOver;
-    let gameOver = true;
-
-    // this.state.score = 0;
-    // let score = this.state.score;
-
-    let score = 0;
-    // this.state.idArr = [];
-    // const idArr = this.state.idArr;
-    const idArr = [];
-
-    // this.shuffleImages();
-    this.setState({ score, idArr, gameOver })
-    // console.log("if statement ", score)
-  } else {
-    const score = this.state.score + 1;
-    const idArr = this.state.idArr.concat(id);
-    this.shuffleImages();
-    this.setState({ score, idArr });
-    if (score >= characters.length) {
-      alert("You won!")
+  handleClick = id => {
+    console.log(id);
+    if (this.state.idArr.includes(id)) {
+      alert(`Game over! You already chose ${this.state.characters.find(character => character.id === id).name}`)
+      let gameOver = true;
+      let score = 0;
+      const idArr = [];
+      this.setState({ score, idArr, gameOver })
+    } else {
+      const score = this.state.score + 1;
+      const idArr = this.state.idArr.concat(id);
+      this.shuffleImages();
+      this.setState({ score, idArr });
+      if (score >= characters.length) {
+        alert("You won!")
+      }
+      console.log("else statement", idArr);
     }
-    console.log("else statement", idArr);
-  }
-  // this.setState(this.state.hasBeenClicked.push(id));
-  // const friends = this.state.friends.filter(friend => friend.id !== id);
-  // Set this.state.friends equal to the new friends array
-  // this.setState({ friends });
-};
+  };
 
-// let bsModal = React.createClass({
-//   componentDidMount(){
-//       $(this.getDOMNode()).modal('show');
-//       $(this.getDOMNode()).on('hidden.bs.modal', this.props.handleHideModal);
-//   }
 
-render() {
-  return (
-    <div className='container'>
-      <Wrapper>
-        {/* {
-          
-          this.state.characters.filter((character) => {
-            character.id === this.state.characters.id
-            return <Modal ></Modal>
-          })
-        } */}
-        <Title score={this.state.score}></Title>
-        {
-          this.state.characters.map((character) => {
-            return <Card
-              key={character.id}
-              name={character.name}
-              id={character.id}
-              handleClick={this.handleClick}
-              image={character.image}
-              occupation={character.occupation}
-              location={character.location}
-            />
-          })
-        }
+  render() {
+    return (
+      <div>
+        <Jumbotron />
+        <div className='container'>
+          <Wrapper>
+
+            <Title score={this.state.score}></Title>
+            {
+              this.state.characters.map((character) => {
+                return <Card
+                  key={character.id}
+                  name={character.name}
+                  id={character.id}
+                  handleClick={this.handleClick}
+                  image={character.image}
+                  occupation={character.occupation}
+                  location={character.location}
+                />
+              })
+            }
           </Wrapper>
-    </div>
-  )
-}
+        </div>
+
+      </div>
+    )
+  }
 }
 
 
---
+++ src/components/jumbotron.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Jumbotron() {
+    return (
+        <div className="Jumbotron text-center">
+            <div className="container">
+                <h1 className="display-4">Memory Game</h1>
+                <p className="lead">Try to only click each picture once. If you click it more than once, you lose. Otherwise, you win!</p>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbotron;
